a1258ff4893c61ee244c1e81ef091267880283c1
joseluisgs <joseluisgonsan@gmail.com>
2021-12-21 13:33:27 +0100

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  plugins: ['vue'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/require-default-prop": "off",
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      Home
+    </router-link> |
+    <router-link to="/about">
+      About
+    </router-link>
+  </div>
+  <router-view v-slot="{ Component }">
+    <transition
+      name="fade"
+      mode="out-in"
+    >
+      <component :is="Component" />
+    </transition>
+  </router-view>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+/* Programamos las transiciones
+https://vuejs.org/v2/guide/transitions.html */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
